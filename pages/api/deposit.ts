ecdf3d3de4128280b3e854e74c5bad0527a4d826
Desmond <shuguang-lv@outlook.com>
2023-04-27 01:16:32 -0700

    feat: added shadcn/ui

--- pages/api/deposit.ts
+++ pages/api/deposit.ts
@@ -1,36 +1,43 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma";
-import { Prisma } from '@prisma/client'
-import * as bcrypt from 'bcrypt';
+import { Prisma } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 import jwt from "@/lib/jwt";
 
 type Data = {
-    balance: Prisma.Decimal
-}
+  balance: Prisma.Decimal;
+};
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ) {
-    return new Promise<void>(async (resolve, reject) => {
-        jwt.authenticate(req, res, async (req: NextApiRequest, res: NextApiResponse, decoded: any) => {
-            const exists = await prisma.bANK_USERS.findFirst({
-                where: {
-                    USER_ID: decoded.userId
-                },
-            });
-            if (!exists) {
-                res.status(403).end();
-            } else {
-                if (!req.body.amount) {
-                    res.status(400).end();
-                } else {
-                    let newBalance = Number(exists.BALANCE) + Number(req.body.amount);
-                    await prisma.bANK_USERS.update({ where: { USER_ID: decoded.userId }, data: { BALANCE: newBalance } });
-                    res.status(200).json({ balance: newBalance })
-                }
-            }
-            resolve();
+  return new Promise<void>(async (resolve, reject) => {
+    jwt.authenticate(
+      req,
+      res,
+      async (req: NextApiRequest, res: NextApiResponse, decoded: any) => {
+        const exists = await prisma.bANK_USERS.findFirst({
+          where: {
+            USER_ID: decoded.userId,
+          },
         });
-    });
+        if (!exists) {
+          res.status(403).end();
+        } else {
+          if (!req.body.amount) {
+            res.status(400).end();
+          } else {
+            let newBalance = Number(exists.BALANCE) + Number(req.body.amount);
+            await prisma.bANK_USERS.update({
+              where: { USER_ID: decoded.userId },
+              data: { BALANCE: newBalance },
+            });
+            res.status(200).json({ balance: newBalance });
+          }
+        }
+        resolve();
+      }
+    );
+  });
 }
