3ac990c9cb5285dc9a15344088ba7a8bb526192e
Desmond <shuguang-lv@outlook.com>
2023-04-28 01:50:34 -0700

    feat: added basic ui components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
+import { Nunito_Sans } from "next/font/google"
+
+const nunito = Nunito_Sans({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-nunito",
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={`${nunito.className} ${nunito.variable} flex h-screen w-screen flex-col items-center justify-center font-sans text-slate-900 antialiased`}
+    >
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -63,7 +63,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["var(--font-nunito)", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
