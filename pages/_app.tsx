55419d777d1e551146d3d2a838c3df7bf84e3919
Desmond <shuguang-lv@outlook.com>
2023-04-28 17:27:13 -0700

    feat: completed mockup of auth and dashboard pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { Nunito_Sans } from "next/font/google"
 
+import { Separator } from "@/components/ui/separator"
+
 const nunito = Nunito_Sans({
   subsets: ["latin"],
   weight: "400",
@@ -14,6 +16,33 @@ export default function App({ Component, pageProps }: AppProps) {
       className={`${nunito.className} ${nunito.variable} flex h-screen w-screen flex-col items-center justify-center font-sans text-slate-900 antialiased`}
     >
       <Component {...pageProps} />
+      <div className="w-full fixed bottom-0 p-8">
+        <Separator className="mb-4" />
+        <div className="flex justify-between p-2">
+          <p className="text-sm text-muted-foreground">
+            © 2023. SWE266P Group8. All rights reserved.
+          </p>
+          <a
+            href="https://github.com/shuguang-lv/bank"
+            rel="noreferrer"
+            target="_blank"
+            className="text-gray-700 transition hover:opacity-75"
+          >
+            <svg
+              className="w-7 h-7"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ pages/dashboard.tsx
@@ -0,0 +1,96 @@
+import Head from "next/head"
+import { useRouter } from "next/router"
+import { CreditCard, Landmark, LogOut } from "lucide-react"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Separator } from "@/components/ui/separator"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+export default function DashboardPage() {
+  const router = useRouter()
+
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      <Head>
+        <title>Bank - Dashboard</title>
+      </Head>
+      <div className="flex justify-between p-4 w-full bg-gray-50">
+        <div className="flex items-center align-middle">
+          <Landmark size={30} className="mr-4" />
+          <h4 className="mt-0">Bank</h4>
+          <Separator orientation="vertical" className="mx-4 h-6" />
+          <h5 className="mt-0">Dashboard</h5>
+        </div>
+        <div className="flex items-center mr-2">
+          <Avatar className="mr-4">
+            <AvatarImage
+              src="https://api.dicebear.com/6.x/notionists-neutral/svg?seed="
+              alt="avatar"
+            />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="ghost">shuguang-lv@outlook.com</Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-50 p-0">
+              <Button variant="outline" onClick={() => router.push("/auth")}>
+                <LogOut className="mr-2 h-4 w-4" />
+                Log out
+              </Button>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+      <Card className="mt-[130px]">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium mt-0">Balance</CardTitle>
+          <CreditCard className="h-5 w-5 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-3xl font-bold mb-8">12,234</div>
+          <Tabs defaultValue="deposit" className="w-[400px]">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="deposit">Deposit</TabsTrigger>
+              <TabsTrigger value="withdraw">Withdraw</TabsTrigger>
+            </TabsList>
+            <TabsContent value="deposit">
+              <Card className="pt-8">
+                <CardContent className="flex w-full max-w-sm items-center space-x-2">
+                  <Input
+                    id="deposit"
+                    type="number"
+                    defaultValue="0"
+                    placeholder="Amount"
+                  />
+                  <Button>Submit</Button>
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="withdraw">
+              <Card className="pt-8">
+                <CardContent className="flex w-full max-w-sm items-center space-x-2">
+                  <Input
+                    id="withdraw"
+                    type="number"
+                    defaultValue="0"
+                    placeholder="Amount"
+                  />
+                  <Button>Submit</Button>
+                </CardContent>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
