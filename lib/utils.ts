c3e9982ff4c0247990ce75733b4a7e1dcc23633b
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2023-05-06 23:27:24 -0700

    server input validation done

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,8 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 const EMAIL_REGX = /^[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i
-const NAME_REGX = /^[a-z0-9_.-]{1,127}$/
-const PWD_REGX = /^[a-z0-9_.-]{1,127}$/
+const NAME_REGX = /^[_\\-\\.0-9a-z]{1,127}$/
+const PWD_REGX = /^[_\\-\\.0-9a-z]{1,127}$/
 const NUMBER_REGX = /^[1-9]\d*(\.\d{2})?$/
 
 export const validateEmail = (email: string) => EMAIL_REGX.test(email.trim())
--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -4,6 +4,7 @@ import * as bcrypt from "bcrypt"
 
 import jwt from "@/lib/jwt"
 import prisma from "@/lib/prisma"
+import { validateBalance, validateName, validatePassword } from "@/lib/utils"
 
 type Data = {
   username: string
@@ -15,38 +16,53 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   return new Promise<void>(async (resolve, reject) => {
-    if (req.method !== "POST" || !req.body.username || !req.body.password) {
+    if (
+      req.method !== "POST" ||
+      !req.body.username ||
+      !req.body.password ||
+      req.body.balance == null
+    ) {
       res.status(400).end()
       resolve()
-    } else {
-      const exists = await prisma.bANK_USERS.findUnique({
-        where: {
-          USER_NAME: req.body.username,
-        },
-      })
-      if (exists) {
-        // user with this email already exists
-        res.status(422).end()
-        resolve()
-      } else {
-        let salt = new Buffer(await bcrypt.genSalt(10))
-        let hash = await argon2.hash(req.body.password, { salt })
-        let balance = req.body.balance ? req.body.balance : 0
-        await prisma.bANK_USERS.create({
-          data: {
-            USER_NAME: req.body.username,
-            PWD_HASH: hash,
-            BALANCE: balance,
-          },
-        })
-        let token = jwt.sign(
-          { username: req.body.username },
-          { expiresIn: "1h" }
-        )
-
-        res.status(200).json({ username: req.body.username, token: token })
-        resolve()
-      }
+      return
     }
+    if (
+      !validateName(req.body.username) ||
+      !validatePassword(req.body.password) ||
+      !validateBalance(req.body.balance)
+    ) {
+      console.log("invalid")
+      console.log(validateName(req.body.username))
+      console.log(validatePassword(req.body.password))
+      console.log(validateBalance(req.body.balance))
+      res.status(422).end()
+      resolve()
+      return
+    }
+    const exists = await prisma.bANK_USERS.findUnique({
+      where: {
+        USER_NAME: req.body.username,
+      },
+    })
+    if (exists) {
+      // user with this email already exists
+      res.status(422).end()
+      resolve()
+      return
+    }
+    let salt = new Buffer(await bcrypt.genSalt(10))
+    let hash = await argon2.hash(req.body.password, { salt })
+    let balance = req.body.balance ? req.body.balance : 0
+    await prisma.bANK_USERS.create({
+      data: {
+        USER_NAME: req.body.username,
+        PWD_HASH: hash,
+        BALANCE: balance,
+      },
+    })
+    let token = jwt.sign({ username: req.body.username }, { expiresIn: "1h" })
+
+    res.status(200).json({ username: req.body.username, token: token })
+    resolve()
   })
 }
