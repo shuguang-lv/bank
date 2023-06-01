514ef88dd80692d9daa6e481b93172d31eb14062
SiyaoIsHiding <siyaoh4@uci.edu>
2023-06-01 23:49:51 +0000

    all endpoints rate limit

--- pages/api/balance.ts
+++ pages/api/balance.ts
@@ -3,16 +3,30 @@ import { Prisma } from "@prisma/client"
 
 import jwt from "@/lib/jwt"
 import prisma from "@/lib/prisma"
+import rateLimit from "@/lib/rate-limiter"
 
 type Data = {
   balance: Prisma.Decimal
 }
+
+const limiter = rateLimit({
+  interval: 60 * 1000, // 60 seconds
+  uniqueTokenPerInterval: 500, // Max 500 users per second
+})
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   return new Promise<void>(async (resolve, reject) => {
+    try{
+      await limiter.check(res, 10, req.body.username)
+    }catch(err){
+      res.status(429).end()
+      resolve()
+      return
+    }
+    
     jwt.authenticate(
       req,
       res,
